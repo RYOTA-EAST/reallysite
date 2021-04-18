28e02db8a3406d367f43f4242fa0c1649d540bf9
RYOTA-EAST <70444118+RYOTA-EAST@users.noreply.github.com>
2021-04-19 00:34:59 +0900

    新規登録

--- mysite/views.py
+++ mysite/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.views import LoginView
 from blog.models import Article
+from mysite.forms import UserCreationForm
+from django.contrib import messages
 
 def index(request):
-  objs = Article.objects.all()
+  objs = Article.objects.all()[:3]
   context = {
     'title': 'Rrally Site',
     'articles': objs,
@@ -12,4 +14,24 @@ def index(request):
   return render(request, 'mysite/index.html', context)
 
 class Login(LoginView):
-  template_name = 'mysite/login.html'
+  template_name = 'mysite/auth.html'
+
+  def form_valid(self, form):
+    messages.success(self.request, 'ログイン完了！！！')
+    return super().form_valid(form)
+
+  def form_invalid(self, form):
+    messages.error(self.request, 'エラーログイン失敗')
+    return super().form_invalid(form)
+
+def signup(request):
+  context = {}
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save(commit=False)
+      # user.is_active = False
+      user.save()
+      messages.success(request, '登録完了！！！')
+      return redirect('/')
+  return render(request, 'mysite/auth.html', context)
