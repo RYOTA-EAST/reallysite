b1f3377d73d1222c9217e4afe0bf1e6ad9881d7f
RYOTA-EAST <70444118+RYOTA-EAST@users.noreply.github.com>
2021-04-24 18:40:25 +0900

    お問い合わせ実装

--- config/settings.py
+++ config/settings.py
@@ -26,8 +26,20 @@ SECRET_KEY = 'django-insecure-(cemm+z(-ww1rwrm@(%1*#)*(s4o^u58l20oxz+&6!3=eil1w5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+
+
 ALLOWED_HOSTS = ['*']
 
+if DEBUG:
+    # 開発環境
+    import yaml
+    with open(os.path.join(BASE_DIR, 'secrets', 'secret_dev.yaml')) as file:
+        objs = yaml.safe_load(file)
+        for obj in objs:
+            os.environ[obj] = objs[obj]
+else:
+    # 本番環境
+    pass
 
 # Application definition
 
@@ -147,3 +159,12 @@ MESSAGE_TAGS = {
     messages.DEBUG: 'rounded-0 alert alert-secondary',
  }
 # --------- massage tab with bootstrap alert class ---------------------
+
+# --- Gmail 送信設定
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
+# --- Gmail 送信設定
