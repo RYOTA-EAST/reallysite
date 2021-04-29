767812626a9dbdee30e6ecb694e9e0305c3251c8
RYOTA-EAST <70444118+RYOTA-EAST@users.noreply.github.com>
2021-04-29 23:56:38 +0900

    GCSへ画像アップロード

--- config/settings.py
+++ config/settings.py
@@ -161,6 +161,16 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# --- GCS設定
+from google.oauth2 import service_account
+
+GS_CREDENTIALS = service_account.Credentials.from_service_account_file(
+    os.path.join(BASE_DIR, 'secrets', os.environ['GCS_CREDENTIALS_FILENAME'])
+)
+DEFAULT_FILE_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
+GS_BUCKET_NAME = os.environ['GCS_BUCKET_NAME']
+GS_PROJECT_ID = os.environ['GCS_PROJECT_ID']
 # --- static設定項目 ---
 
 # Default primary key field type
