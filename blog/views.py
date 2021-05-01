a72283337e76370a591e3295137663c7072faa7d
RYOTA-EAST <70444118+RYOTA-EAST@users.noreply.github.com>
2021-05-01 16:22:44 +0900

    いいねの非同期通信

--- blog/views.py
+++ blog/views.py
@@ -48,3 +48,16 @@ def tags(request, slug):
     'page_number': page_number,
   }
   return render(request, 'blog/blogs.html', context)
+
+from django.views.decorators.csrf import ensure_csrf_cookie
+from django.http import JsonResponse
+
+@ensure_csrf_cookie
+def like(request, pk):
+  d = {"message": "error"}
+  if request.method == 'POST':
+    obj = Article.objects.get(pk=pk)
+    obj.count += 1
+    obj.save()
+    d["message"] = "success"
+  return JsonResponse(d)
